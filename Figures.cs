0a9ef718469c99d476bb28d5dc0a2bbe78a55c9c
Владимир Куклин <102403536+VK85111@users.noreply.github.com>
2022-04-06 23:52:59 +0300

    Update Figures.cs
    Убрал ненужное

--- Figures.cs
+++ Figures.cs
@@ -1,23 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace FiguresList
 {
     public class Figures
     {
     }
 
-    public class Triangle: Figures
+    public class Triangle : Figures
     {
         public double side_a { get; set; }
         public double side_b { get; set; }
         public double side_c { get; set; }
     }
 
-    public class Circle: Figures
+    public class Circle : Figures
     {
         public double radius { get; set; }
     }
