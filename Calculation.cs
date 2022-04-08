d29663e95073a1c3dba1c7c5123aa24064a20ca9
Владимир Куклин <102403536+VK85111@users.noreply.github.com>
2022-04-08 16:23:04 +0300

    Update Calculation.cs
    Улучшения

--- Calculation.cs
+++ Calculation.cs
@@ -7,7 +7,7 @@ namespace Calculation
     {
         public double Calc(Figures obj)
         {
-            double reslt = 0; 
+            double reslt = 0;
             if (obj is Circle)
             {
                 reslt = CircleCalc(obj as Circle);
@@ -16,29 +16,42 @@ namespace Calculation
             {
                 reslt = TriangleCalc(obj as Triangle);
             }
+            else if (obj == null)
+            {
+                throw new Exception("Тип фигуры не известен");
+            }
 
             return reslt;
+
+            //Для новых версий языка
+            //if (obj == null) throw new Exception("Фигура не может быть null");
+            //return obj switch
+            //{
+            //    Circle circle => CircleCalc(obj),
+            //    Triangle triangle => TriangleCalc(obj),
+            //    _ => throw new Exception("Тип фигуры не известен")
+            //};
 
         }
 
         public double CircleCalc(Circle circl)
         {
-            return Math.PI * Math.Pow(circl.radius, 2);
+            return Math.PI * Math.Pow(circl.Radius, 2);
         }
 
         public double TriangleCalc(Triangle triangl)
         {
-            double halfPerimeter = (triangl.side_a + triangl.side_b + triangl.side_c) / 2;
+            double halfPerimeter = (triangl.SideA + triangl.SideB + triangl.SideC) / 2;
             return Math.Sqrt(halfPerimeter
-                        * (halfPerimeter - triangl.side_a)
-                        * (halfPerimeter - triangl.side_b)
-                        * (halfPerimeter - triangl.side_c));
+                        * (halfPerimeter - triangl.SideA)
+                        * (halfPerimeter - triangl.SideB)
+                        * (halfPerimeter - triangl.SideC));
         }
 
         public bool TriangleCheck(Triangle triangl)
         {
-            return Math.Pow(triangl.side_a, 2) 
-                + Math.Pow(triangl.side_b, 2) == Math.Pow(triangl.side_c, 2);
+            return Math.Pow(triangl.SideA, 2) 
+                + Math.Pow(triangl.SideB, 2) == Math.Pow(triangl.SideC, 2);
         }
 
     }
